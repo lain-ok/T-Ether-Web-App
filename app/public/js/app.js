cdec510cfc779218e60018ce990a33292a0d2ba9
Will Kopil <49411638+WillKopil@users.noreply.github.com>
2022-03-23 11:04:33 -0400

    added geolocation

--- app/public/js/app.js
+++ app/public/js/app.js
@@ -6,6 +6,24 @@ var map = L.map('map');
             attribution: 'Map data &copy; OpenStreetMap contributors'
         }).addTo(map);
 
-        var marker = L.marker([51.2, 7]).addTo(map);
+var sidebar = L.control.sidebar('sidebar').addTo(map);
 
-        var sidebar = L.control.sidebar('sidebar').addTo(map);
+var marker = L.marker([51.2, 7]).addTo(map);
+
+function onLocationFound(e) {
+        var radius = e.accuracy / 10;
+
+        L.marker(e.latlng).addTo(map)
+                .bindPopup("You are within " + radius + " meters from this point").openPopup();
+
+        L.circle(e.latlng, radius).addTo(map);
+}
+
+function onLocationError(e) {
+        alert(e.message);
+}
+
+map.on('locationfound', onLocationFound);
+map.on('locationerror', onLocationError);
+
+map.locate({setView: true, maxZoom: 16});
