7cc376a4d062f5b4e85b529efdf435883cc34a07
24momo <malek.b98@hotmail.com>
2022-03-28 16:32:34 +0000

    period

--- app/public/js/app.js
+++ app/public/js/app.js
@@ -14,8 +14,8 @@ function onLocationFound(e) {
         var radius = e.accuracy / 10;
 
         L.marker(e.latlng).addTo(map)
-                .bindPopup("You are within " + radius + " meters from this point").openPopup();
-
+                .bindPopup("You are within " + radius + " meters from this point.").openPopup();
+                
         L.circle(e.latlng, radius).addTo(map);
 }
 
